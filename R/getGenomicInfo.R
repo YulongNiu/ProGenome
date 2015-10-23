795a2f2ff6fe1067d8745f24cbfb6725a780d6a2
YulongNiu <niuylscu@gmail.com>
2015-10-24 00:09:06 +0800

    get genomic genes from FTP url

--- R/getGenomicInfo.R
+++ R/getGenomicInfo.R
@@ -3,6 +3,7 @@
 ##' getFtpUrl(): get the FTP url storing genome information from GenBank or RefSeq database. This function is mainly used to get the species genome assembly information.
 ##' listFtpFileUrl(): list the file download FTP urls.
 ##' read.gffUrl(): read in raw gff file from a FTP url.
+##' getLocsfgff(): get genomic gene location information from gff file.
 ##' @title Retrieve genome FTP URL
 ##' @inheritParams getGenomicGenes
 ##' @param database "GenBank" or "RefSeq".
@@ -93,14 +94,114 @@ listFileFtpUrl <- function(ftpUrl) {
 ##'
 ##' 
 read.gffUrl <- function(gffUrl) {
-  
+
+  ## read gz files from stream
   conStream <- gzcon(url(gffUrl))
-  
   eachLine <- readLines(conStream)
-  
-  gffMat <- read.table(textConnection(eachLine), sep = '\t', quote = '', stringsAsFactors = FALSE)
+  gffMat <- read.table(textConnection(eachLine),
+                       sep = '\t',
+                       quote = '',
+                       stringsAsFactors = FALSE)
+
+  ## remove rows and columns names
+  row.names <- NULL
+  col.names <- NULL
 
   return(gffMat)
   
 }
 
+
+##' @inheritParams getGenomicGenes
+##' @return A list of genomes containing gene location information. The locus_tags is used for the gene names.
+##' @examples
+##' draLocs <- getLocsfgff('dra')
+##' \dontrun{
+##' hxaLocs <- getLocsfgff('hxa')
+##' csuLocs <- getLocsfgff('csu')}
+##' @rdname genomeFTP
+##' @author Yulong Niu \email{niuylscu@@gmail.com}
+##' @export
+##'
+##' 
+getLocsfgff <- function(KEGGSpe) {
+
+
+  getLocsTag <- function(annoStr) {
+    ## USE: extract locus_tags from a gff annotation information.
+    ## INPUT: character strings of gff annotation, which is sperated by ';'.
+    ## OUTPU: locus_tags
+    ## EXAMPLE:
+    ## testStr <- c('ID=gene1;Dbxref=GeneID:10795663;Name=HALXA_RS00010;gbkey=Gene;gene_biotype=protein_coding;locus_tag=HALXA_RS00010;old_locus_tag=Halxa_0687', 'ID=gene0;Dbxref=GeneID:10799347;Name=HALXA_RS00005;gbkey=Gene;gene_biotype=protein_coding;locus_tag=HALXA_RS00005;old_locus_tag=Halxa_0686')
+    ## testLocs <- getLocsTag(testStr)
+
+    locusSplit <- strsplit(annoStr, split = ';')
+    locusTag <- sapply(locusSplit, function(x) {
+      eachLocus <- x[grepl('^locus_tag=', x)]
+      eachLocus <- sapply(strsplit(eachLocus, split = '=', fixed = TRUE), '[[', 2)
+
+      return(eachLocus)
+    })
+    
+    return(locusTag)
+  }
+
+ ExtractLocs <- function(gffMat) {
+    ## USE: extract gene location from gff raw file.
+    ## INPUT: 'gffMat' is the gff matrix.
+    ## OUTPUT: 4-column matrix, 1st is the locus_tags, 2ed is the from locus, 3rd is the to locus, 4th is the strand.
+
+    geneLogic <- gffMat[, 3] == 'gene'
+    geneMat <- gffMat[geneLogic, ]
+
+    locMat <- cbind(getLocsTag(geneMat[, 9]),
+                    geneMat[, 4],
+                    geneMat[, 5],
+                    geneMat[, 7],
+                    deparse.level = 0)
+    
+    colnames(locMat) <- c('GeneName', 'From', 'To', 'Strand')
+    rownames(locMat) <- NULL
+
+    return(locMat)
+  }
+
+  
+  ##~~~~~~~~~~~~~~~~~~~~read in gff files~~~~~~~~~~~~~~~~~~
+  speUrl <- getSpeFtpUrl(KEGGSpe, database = 'RefSeq')
+  speFiles <- listFileFtpUrl(speUrl)
+  gffUrl <- speFiles[grepl('gff', speFiles)]
+  spegff <- read.gffUrl(gffUrl)
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  ##~~~~~~~~~~~~~~~~get gene locs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ## identify using feature "Is_circular"
+  gTagIdx <- which(grepl('Is_circular=', spegff[, 9]))
+  gTag <- spegff[gTagIdx, 9]
+  pLoc <- grepl('plasmid', gTag)
+  if (sum(pLoc) > 0) {
+    ## must have one genome
+    gNames <- c(paste0('genome', 1:sum(!pLoc)),
+                paste0('plasmid', 1:sum(pLoc)))
+  } else {
+    gNames <- paste0('genome', 1:sum(!pLoc))
+  }
+
+
+  ## split genomes
+  ## length(pLoc) == nrow(gMat) is TRUE
+  regionLoc <- c(gTagIdx, nrow(spegff))
+  startVec <- regionLoc[-length(regionLoc)] + 1
+  endVec <- regionLoc[-1] - 1
+  gMat <- cbind(startVec, endVec)
+
+  gList <- vector('list', length(gNames))
+  for (i in 1:nrow(gMat)) {
+    gList[[i]] <- ExtractLocs(spegff[gMat[i, 1] : gMat[i, 2], ])
+  }
+  names(gList) <- gNames
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  return(gList)
+}
+  
