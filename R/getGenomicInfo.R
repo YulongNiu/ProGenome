2d84e92322f1f3532c915d40c5cdfa17e2d0eda0
YulongNiu <niuylscu@gmail.com>
2015-10-24 01:47:17 +0800

    find RefSeq and GenBank FTP URL at the same time

--- R/getGenomicInfo.R
+++ R/getGenomicInfo.R
@@ -3,16 +3,16 @@
 ##' getFtpUrl(): get the FTP url storing genome information from GenBank or RefSeq database. This function is mainly used to get the species genome assembly information.
 ##' listFtpFileUrl(): list the file download FTP urls.
 ##' read.gffUrl(): read in raw gff file from a FTP url.
-##' getLocsfgff(): get genomic gene location information from gff file.
+##' getLocsfgff(): get genomic gene location information from gff file. It trys the RefSeq database at first; if RefSeq is not found, then change the database to GenBank.
 ##' @title Retrieve genome FTP URL
 ##' @inheritParams getGenomicGenes
-##' @param database "GenBank" or "RefSeq".
+##' @param database "GenBank", "RefSeq", or c("GenBank", "RefSeq").
 ##' @return FTP url.
 ##' @examples
 ##' ## E. coli genome GenBank FTP
-##' ecoliUrl <- getSpeFtpUrl('eco')
+##' ecoliUrl <- getSpeFtpUrl('eco', c('GenBank', 'RefSeq'))
 ##' ## list E. coli FTP files
-##' ecoliFiles <- listFileFtpUrl(ecoliUrl)
+##' ecoliFiles <- listFileFtpUrl(ecoliUrl[1])
 ##' ## read in gff file
 ##' gffUrl <- ecoliFiles[grepl('gff', ecoliFiles)]
 ##' ecoligff <- read.gffUrl(gffUrl)
@@ -24,6 +24,7 @@
 ##' @importFrom stringr str_extract str_detect
 ##' @importFrom RCurl getURL
 ##' @importFrom xml2 read_xml xml_attr
+##' @importFrom foreach foreach %do%
 ##' @author Yulong Niu \email{niuylscu@@gmail.com}
 ##' @rdname genomeFTP
 ##' @export
@@ -41,21 +42,33 @@ getSpeFtpUrl <- function(KEGGSpe, database = 'GenBank') {
   ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   ##~~~~~~~~~~~~~~~~~~~~~~FTP URL~~~~~~~~~~~~~~~~~~~
+  ## collapse database
+  database <- paste(database, collapse = '|')
+  
   ## NCBI assembly url
   assBaseUrl <- 'http://www.ncbi.nlm.nih.gov/assembly/'
   assUrl <- paste0(assBaseUrl, assNum)
 
   webStr <- getURL(assUrl)
   webVec <- unlist(strsplit(webStr, split = '\n', fixed = TRUE))
-  matchPattern <- paste0(database, ' FTP site')
+  matchPattern <- paste0('(', database, ')', ' FTP site')
   ftpLine <- webVec[str_detect(webVec, matchPattern)]
 
   ## find url
-  ftpXml <- read_xml(ftpLine)
-  ftpUrl <- xml_attr(ftpXml, 'href')
-
+  ftpUrl <- foreach(i = 1:length(ftpLine), .combine = c) %do% {
+    eachFtpXml <- read_xml(ftpLine[i])
+    eachFtpUrl <- xml_attr(eachFtpXml, 'href')
+    eachName <- sapply(strsplit(xml_text(eachFtpXml), ' ', fixed = TRUE),
+                       '[[',
+                       1)
+    names(eachFtpUrl) <- eachName
+    return(eachFtpUrl)
+  }
+  
   ## ftp url often point to a folder
+  ftpUrlNames <- names(ftpUrl)
   ftpUrl <- paste0(ftpUrl, '/')
+  names(ftpUrl) <- ftpUrlNames
   ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
   return(ftpUrl)
